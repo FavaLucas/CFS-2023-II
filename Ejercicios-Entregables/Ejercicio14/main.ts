868e79a05b9896f3afadb6a97137202324c29950
Lucas Fava <lucasfava86@gmail.com>
2023-09-19 08:39:49 -0300

    Ejercicios Entregables

--- Ejercicios-Entregables/Ejercicio14/main.ts
+++ Ejercicios-Entregables/Ejercicio14/main.ts
@@ -2,14 +2,12 @@ import { mamifero } from "./mamifero";
 import { reptil } from "./reptil";
 import { murciegalo } from "./murciegalo";
 
-let golbat = new murciegalo("Golbat",74,"Violeta","Onda Sonica");
+let golbat = new murciegalo("Batman",74,"Violeta","Onda Sonica");
 golbat.utilizarEcolocalizacion();
 
-let pikachu = new mamifero("Pikachu", 105,"Amarillo");
+let pikachu = new mamifero("El raton perez", 105,"Amarillo");
 pikachu.comer();
-console.log("Pikachu, ATACK TRUENOOO!!!!")
 
 
-let ekans = new reptil ("Ekans", 25,"Violeta");
+let ekans = new reptil ("Reptilo", 25,"Violeta");
 ekans.tempCorporal();
-console.log("Ekans, utiliza Picotazo Venenoso.")
